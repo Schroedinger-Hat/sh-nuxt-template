67671485928d26289a343b099ab0ee7b04812920
Patrick Raedler <code@readpato.dev>
2023-11-26 22:32:28 +0100

    chore: install unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@unocss/nuxt'],
+  css: ['@unocss/reset/tailwind.css'],
 })
---
+++ uno.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, presetAttributify, presetUno, presetWebFonts } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetWebFonts(),
+  ],
+})
