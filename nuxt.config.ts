c8aa7585467e3ad81c059996bb83c37e3d5b2e8f
Patrick Raedler <code@readpato.dev>
2023-11-28 21:15:42 +0100

    chore(deps): install vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt'],
+  modules: ['@unocss/nuxt', '@vueuse/nuxt'],
   css: ['@unocss/reset/tailwind.css'],
 })
+
