95197b8fe91fcc58d70eb6ae5be63c631b2bf141
Patrick Raedler <code@readpato.dev>
2023-11-28 22:55:31 +0100

    chore(deps): install nuxt-svgo module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt', '@vueuse/nuxt', 'nuxt-vitest'],
+  modules: ['@unocss/nuxt', '@vueuse/nuxt', 'nuxt-vitest', 'nuxt-svgo'],
   css: ['@unocss/reset/tailwind.css'],
+  svgo: {
+    defaultImport: 'component',
+  },
 })
+
