29d8b8b8c1a11e6020664a28487154f771685cf0
Patrick Raedler <code@readpato.dev>
2024-07-13 15:34:09 +0200

    feat(CtaColorMode/Icon): create base components

---
+++ components/CtaIcon.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { ButtonVariants } from '~/components/ui/button'
+
+withDefaults(defineProps<{
+  asChild?: boolean
+  icon: string
+  isAnchor?: boolean
+  to?: string
+  variant?: ButtonVariants['variant']
+}>(), {
+  variant: 'outline',
+})
+</script>
+
+<template>
+  <SCButton size="icon" :variant="variant" class="size-8" :as-child="asChild">
+    <template v-if="asChild && isAnchor">
+      <NuxtLink :to="to">
+        <Icon :name="`carbon:${icon}`" class="size-5" />
+      </NuxtLink>
+    </template>
+    <template v-else>
+      <Icon :name="`carbon:${icon}`" class="size-5" />
+    </template>
+  </SCButton>
+</template>
